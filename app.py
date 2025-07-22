4eef50858c85abd459ec6b41b3a09ebedbb334fc
Malheiros76 <l_souza.malheiros@hotmail.com>
2025-07-22 10:39:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,42 @@ if not st.session_state.logado:
             else:
                 st.error("Usuário ou senha inválidos.")
 
+mensagens = alerta_estoque(movimentacao_col)
 
+def alerta_estoque():
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$produto",
+                "entrada": {"$sum": {"$cond": [{"$eq": ["$tipo", "Entrada"]}, "$quantidade", 0]}},
+                "saida": {"$sum": {"$cond": [{"$eq": ["$tipo", "Saída"]}, "$quantidade", 0]}}
+            }
+        }
+    ]
+    resultados = list(movimentacao_col.aggregate(pipeline))
+    mensagens = []
+    for r in resultados:
+        saldo = r["entrada"] - r["saida"]
+        limite = r["entrada"] * 0.2
+        if saldo < limite:
+            mensagens.append(f"Produto {r['_id']} está abaixo do limite. Saldo atual: {saldo}")
+    return mensagens
+
+def enviar_email(destinatario, mensagem):
+    try:
+        msg = MIMEText(mensagem)
+        msg['Subject'] = 'Alerta de Estoque Baixo'
+        msg['From'] = 'bibliotecaluizcarlos@gmail.com'
+        msg['To'] = destinatario
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login('bibliotecaluizcarlos@gmail.com', 'terra166') # senha app se precisar
+            server.send_message(msg)
+    except Exception as e:
+        st.error(f"Erro ao enviar email: {e}")
+
+def enviar_whatsapp(numero, mensagem):
+    numero = numero.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+    texto = urllib.parse.quote(mensagem)
+    url = f"https://wa.me/55{numero}?text={texto}"
+    st.markdown(f"[Abrir WhatsApp]({url})")
