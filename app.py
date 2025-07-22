f71929fee3892385e56b7deffc0f667d9687f292
Malheiros76 <l_souza.malheiros@hotmail.com>
2025-07-22 10:34:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from auth import autenticar
 from db import get_db
 
+# bancos de  dados
 db = get_db()
 usuarios_col = db["usuarios"]
 cadastro_col = db["cadastro"]
@@ -7,3 +9,20 @@ produtos_col = db["produtos"]
 movimentacao_col = db["movimentacao"]
 alunos_col = db["alunos"]
 movimentacao_aluno_col = db["movimentacao_aluno"]
+
+# Autenticação
+
+if "logado" not in st.session_state:
+    st.session_state.logado = False
+
+if not st.session_state.logado:
+    st.subheader("Login do Sistema")
+    with st.form("login"):
+        usuario = st.text_input("Usuário")
+        senha = st.text_input("Senha", type="password")
+        if st.form_submit_button("Entrar"):
+            if autenticar(usuario.strip(), senha.strip(), usuarios_col):
+                st.success("Login realizado com sucesso!")
+                st.rerun()
+            else:
+                st.error("Usuário ou senha inválidos.")
